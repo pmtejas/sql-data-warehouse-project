fee14ebeb0c8b92deca97ce63fcd84abaf10af46
thejus p m <92338343+pmtejas@users.noreply.github.com>
2025-07-16 12:25:09 +0530

    Add files via upload

---
+++ scripts/bronze/execute_script.sql
@@ -0,0 +1,6 @@
+DECLARE @RC int
+
+-- TODO: Set parameter values here.
+
+EXECUTE @RC = [bronze].[load_bronze] 
+GO
---
+++ scripts/silver/execute_scriptt.sql
@@ -0,0 +1,6 @@
+DECLARE @RC int
+
+-- TODO: Set parameter values here.
+
+EXECUTE @RC = [silver].[load_silver] 
+GO
